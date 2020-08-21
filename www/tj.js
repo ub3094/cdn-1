6c577f31122f700538a2538c7aa02c17abe39cee
wCloud <62798721+fulidhw@users.noreply.github.com>
2020-08-21 21:01:34 +0800

    Create tj.js

---
+++ www/tj.js
@@ -0,0 +1,2 @@
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/20676637.js"></script>');
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/20522817.js"></script>');
