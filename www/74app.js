344d5b2318a5365b5a719b2c0f19a9bb30a9c4e0
wCloud <62798721+fulidhw@users.noreply.github.com>
2020-08-22 14:26:19 +0800

    Update 74app.js

--- www/74app.js
+++ www/74app.js
@@ -60,4 +60,3 @@ document.writeln("body{overflow:hidden !important}");
 document.writeln("body > div{display:none}");
 document.writeln("</style>");
 document.writeln(" <meta name=\'viewport\' content=\'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0\'>");
- 
