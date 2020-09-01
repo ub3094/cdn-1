faafa9e12392f72555433410a931cd4e076962ae
wCloud <62798721+fulidhw@users.noreply.github.com>
2020-09-01 12:47:01 +0800

    Update tj.js

--- www/tj.js
+++ www/tj.js
@@ -1,2 +1,3 @@
 document.write ('<script type="text/javascript"  src="https://js.users.51.la/20676637.js"></script>');
 document.write ('<script type="text/javascript"  src="https://js.users.51.la/20522817.js"></script>');
+document.write ('<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1279231050'%3E%3C/span%3E%3Cscript src='https://v1.cnzz.com/z_stat.php%3Fid%3D1279231050' type='text/javascript'%3E%3C/script%3E"));</script>');
