710172d00d6d7959b28856f3301bbdc4714182e2
wCloud <62798721+fulidhw@users.noreply.github.com>
2020-09-08 00:31:51 +0800

    Update app.js

--- 91app/TC/app.js
+++ 91app/TC/app.js
@@ -1,6 +1,6 @@
 var myDate = new Date();
 var time =  myDate.getFullYear() +""+ myDate.getMonth() +""+myDate.getDate() +""+ myDate.getHours();
-document.writeln("<script src=\'https://ios.android.totoroman.com/TC/TC/js/jquery-3.3.1.min.js?"+time+"\'></script>");
-document.writeln("<script src=\'https://ios.android.totoroman.com/TC/TC/js/clipboard.min.js?"+time+"\'></script>"); 
-document.writeln("<script src=\'https://ios.android.totoroman.com/TC/TC/js/ft.js?"+time+"\'></script>"); 
-document.writeln("<link rel=\'stylesheet\' type=\'text/css\' href=\'https://ios.android.totoroman.com/TC/TC/css/huola.css\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/fulidhw/cdn/91app/TC/js/jquery-3.3.1.min.js?"+time+"\'></script>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/fulidhw/cdn/91app/TC/js/clipboard.min.js?"+time+"\'></script>"); 
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/fulidhw/cdn/91app/TC/js/ft.js?"+time+"\'></script>"); 
+document.writeln("<link rel=\'stylesheet\' type=\'text/css\' href=\'https://cdn.jsdelivr.net/gh/fulidhw/cdn/91app/TC/css/app.css\'>");
